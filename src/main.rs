9bd0b45f102764a69dcd22197571e50ff6796f46
Francis Chua <francis@opennetworking.org>
2021-11-07 15:30:45 -0800

    formatted

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate autopilot;
-use autopilot::mouse;
 use autopilot::alert::alert;
+use autopilot::mouse;
 
 fn click(count: u32, delay: u64, left_not_right: bool) {
     for _ in 0..count {
@@ -15,10 +15,26 @@ fn click(count: u32, delay: u64, left_not_right: bool) {
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let rounds = args.get(1).expect("Pass the number of rounds").parse::<u64>().expect("Pass an integer value");
-    let clicks_per_round = args.get(2).expect("Pass the number of clicks per round").parse::<u32>().expect("Pass an integer value");
-    let delay_between_rounds = args.get(3).expect("Pass the delay between rounds in milliseconds").parse::<u64>().expect("Pass an integer value");
-    let delay_for_click = args.get(4).expect("Pass the delay between clicks in milliseconds").parse::<u64>().expect("Pass an integer value");
+    let rounds = args
+        .get(1)
+        .expect("Pass the number of rounds")
+        .parse::<u64>()
+        .expect("Pass an integer value");
+    let clicks_per_round = args
+        .get(2)
+        .expect("Pass the number of clicks per round")
+        .parse::<u32>()
+        .expect("Pass an integer value");
+    let delay_between_rounds = args
+        .get(3)
+        .expect("Pass the delay between rounds in milliseconds")
+        .parse::<u64>()
+        .expect("Pass an integer value");
+    let delay_for_click = args
+        .get(4)
+        .expect("Pass the delay between clicks in milliseconds")
+        .parse::<u64>()
+        .expect("Pass an integer value");
     let left_not_right = args.get(5).map(|a| a != "right").unwrap_or(true);
     println!("Clicking {} times per each round of {} with a {} ms delay between rounds and a {} ms delay between clicks.", clicks_per_round, rounds, delay_between_rounds, delay_for_click);
     for _ in 0..rounds {
@@ -26,5 +42,13 @@ fn main() {
         std::thread::sleep(std::time::Duration::from_millis(delay_between_rounds));
     }
 
-    alert(&format!("Clicking {} times per each round of {} with a {} ms delay between rounds.", clicks_per_round, rounds, delay_between_rounds), Some("Done"), None, None);
+    alert(
+        &format!(
+            "Clicking {} times per each round of {} with a {} ms delay between rounds.",
+            clicks_per_round, rounds, delay_between_rounds
+        ),
+        Some("Done"),
+        None,
+        None,
+    );
 }
